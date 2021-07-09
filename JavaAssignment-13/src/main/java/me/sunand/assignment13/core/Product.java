7900e6ede41c27bdef4ab358873fc38c6dd04ad2
sunandsharma <sunand.sharma@zopsmart.com>
2021-07-08 23:49:38 -0700

    Product functionality using Cassandra.

---
+++ JavaAssignment-13/src/main/java/me/sunand/assignment13/core/Product.java
@@ -0,0 +1,11 @@
+package me.sunand.assignment13.core;
+public class Product {
+    Product(int id,String product_name,int cost,int quantity){
+        this.id=id;
+        this.product_name=product_name;
+        this.cost=cost;
+        this.quantity=quantity;
+    }
+    int id,cost,quantity;
+    String product_name;
+}
